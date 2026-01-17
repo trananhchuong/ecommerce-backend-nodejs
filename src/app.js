1c9a006aebc302407d0c0003354d9d5ad5e84db1
chapter <chuongninolxag@gmail.com>
2026-01-17 13:38:52 +0700

    feat: add compression lib

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const helmet = require("helmet");
+const compression = require("compression");
 
 // init middleware
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 // app.use(morgan("combined"));
 // app.use(morgan("short"));
 // app.use(morgan("common"));
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(helmet());
+app.use(compression());
 // init database
 
 // init routes
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello World" });
+  const strCompress = "Hello World";
+  return res
+    .status(200)
+    .json({ message: "Hello World", metadata: strCompress.repeat(1000000) });
 });
 
 // error handling

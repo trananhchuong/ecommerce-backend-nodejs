1ce8b832d66a461295fef6c5a4b3a19e5fff77e6
chapter <chuongninolxag@gmail.com>
2026-01-17 10:28:32 +0700

    feat: add morgan package

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 
 // init middleware
-
+// app.use(morgan("dev"));
+// app.use(morgan("combined"));
+// app.use(morgan("short"));
+// app.use(morgan("common"));
+app.use(morgan("tiny"));
 
 // init database
 
-
 // init routes
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello World" });
+});
 
 // error handling
 
